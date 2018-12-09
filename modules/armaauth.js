9d849e110ab8bf135f494e56b35d66ac64ceb237
Aaditya Chakravarty <ac36500@gmail.com>
2018-12-09 08:30:31 +0530

    Confidate

--- modules/armaauth.js
+++ modules/armaauth.js
@@ -1,22 +1,22 @@
-const authUser = require('./authUser');
+const md5 = require('md5');
+
+const userModel = require('../models/users');
 
 const auth = (req, res) => {
     authority = req.headers.host;
 
-    let data = req.query;
-
     const getPrefix = () => ("%u:aaauth:");
-    const getSuffix = () => (":"+authority);
+    const getSuffix = () => (":" + authority);
 
     const getMethods = () => {
         res.send('METHODS md5, bmd5');
     }
 
     const getParams = () => {
-        if (data.method == 'md5') {
+        if (req.query.method == 'md5') {
             res.send(`PREFIX ${getPrefix()}\nSUFFIX ${getSuffix()}`);
         }
-        else if (data.method == 'bmd5') {
+        else if (req.query.method == 'bmd5') {
             res.send('');
         }
         else {
@@ -28,13 +28,44 @@ const auth = (req, res) => {
         res.status(404).send(`UNKNOWN_QUERY`);
     }
 
+    const authUser = () => {
+        userModel.findOne({ "username": req.query.user }, (err, doc) => {
+            if (err) {
+                res.status(404).send(`SYSTEM_ERROR`);
+            }
+            else {
+                if (!doc) {
+                    res.status(404).send(`UNKNOWN_USER ${req.query.user}`);
+                }
+                else {
+                    let password = getPrefix().replace("%u", req.query.user) + doc.password + getSuffix();
+                    let passHash = md5(password);
+
+                    let hexSalt = new Buffer(req.query.salt || "", "hex");
+                    let hexHash = new Buffer(passHash || "", "hex");
+
+                    let hexArr = Buffer.concat([hexHash, hexSalt]);
+
+                    let finalPass = md5(hexArr);
+
+                    if (finalPass.localeCompare(req.query.hash) == 0) {
+                        res.status(200).send(`PASSWORD_OK ${data.user}@${authority} \nFOO baz`);
+                    }
+                    else {
+                        res.status(401).send('PASSWORD_FAIL');
+                    }
+                }
+            }
+        });
+    }
+
     let getAction = {
         "methods": getMethods,
         "params": getParams,
         "check": authUser,
         "default": execDef
     };
-    getAction[req.query.query || 'default'](req, res);
+    getAction[req.query.query || 'default']();
 }
 
 module.exports = auth;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/track', require('../modules/getTracks'));
+// router.get('/track', require('../modules/getTracks'));
 router.post('/register', require('../modules/addUser'))
 
 router.use((req, res, next) => {
